463c23e7e36b93951e40f928b1289dccda0c84fd
unknown <diloch111@gmail.com>
2024-09-26 20:01:22 +0100

    refactor current tabs constant in js file

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-let leadsList = []
+let leadsList = [];
 const inputBtnEl = document.getElementById("input-btn");
 const tabBtnEl = document.getElementById("tab-btn");
 const clearAllEl = document.getElementById("clear-all");
 const inputAreaEl = document.getElementById("input-area");
 const leadsAreaEl = document.getElementById("leads-area");
-let leadsFromLocal = JSON.parse(localStorage.getItem('leadsList'))
+let leadsFromLocal = JSON.parse(localStorage.getItem("leadsList"));
 
 if (leadsFromLocal) {
-  leadsList = leadsFromLocal
-  renderLeads(leadsList)
+  leadsList = leadsFromLocal;
+  renderLeads(leadsList);
 }
 
-inputBtnEl.addEventListener('click', () =>{
+inputBtnEl.addEventListener("click", () => {
   if (inputAreaEl.value) {
-    leadsList.push(inputAreaEl.value)
-    inputAreaEl.value = ''
-    localStorage.setItem('leadsList', JSON.stringify(leadsList))
-    renderLeads(leadsList)  
+    leadsList.push(inputAreaEl.value);
+    inputAreaEl.value = "";
+    localStorage.setItem("leadsList", JSON.stringify(leadsList));
+    renderLeads(leadsList);
   }
-})
+});
 
-clearAllEl.addEventListener('click', ()=>{
-  localStorage.clear()
-  leadsList = []
-  renderLeads(leadsList)
-})
+clearAllEl.addEventListener("click", () => {
+  localStorage.clear();
+  leadsList = [];
+  renderLeads(leadsList);
+});
 
-tabBtnEl.addEventListener('click', () => {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+tabBtnEl.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
     if (currentTab) {
-      leadsList.push(currentTab.url)
-      localStorage.setItem('leadsList', JSON.stringify(leadsList))
-      renderLeads(leadsList)
+      leadsList.push(currentTab.url);
+      localStorage.setItem("leadsList", JSON.stringify(leadsList));
+      renderLeads(leadsList);
     }
   });
 });
 
-
 function renderLeads(leads) {
-  leadsAreaEl.textContent = ''
+  leadsAreaEl.textContent = "";
   for (let i = 0; i < leads.length; i++) {
-    let ulEl = document.createElement('ul')
-    let liEl = document.createElement('li')
-    let anchorEl = document.createElement('a')
-    anchorEl.setAttribute("href", leads[i])
-    anchorEl.textContent = leads[i]
+    let ulEl = document.createElement("ul");
+    let liEl = document.createElement("li");
+    let anchorEl = document.createElement("a");
+    anchorEl.setAttribute("href", leads[i]);
+    anchorEl.textContent = leads[i];
     // append to dom
-    liEl.append(anchorEl)
-    ulEl.append(liEl)
-    leadsAreaEl.append(ulEl)  
+    liEl.append(anchorEl);
+    ulEl.append(liEl);
+    leadsAreaEl.append(ulEl);
   }
- }
+}
