2566477f4c8b464e3c274fe27c08a174636b5613
unknown <diloch111@gmail.com>
2024-09-26 14:33:54 +0100

    logic to add to local and display to dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
+let leadsList = []
 const inputBtnEl = document.getElementById("input-btn");
 const tabBtnEl = document.getElementById("tab-btn");
 const clearAllEl = document.getElementById("clear-all");
 const inputAreaEl = document.getElementById("input-area");
 const leadsAreaEl = document.getElementById("leads-area");
+const leadsFromLocal = JSON.parse(localStorage.getItem('leadsList'))
 
-let leadsList = []
+if (leadsFromLocal) {
+  leadsList = leadsFromLocal
+  renderLeads(leadsList)
+}
 
 inputBtnEl.addEventListener('click', () =>{
-  
+  if (inputAreaEl.value) {
+    leadsList.push(inputAreaEl.value)
+    inputAreaEl.value = ''
+    localStorage.setItem('leadsList', JSON.stringify(leadsList))
+    renderLeads(leadsList)  
+  }
 })
+
+
+function renderLeads(leads) {
+  leadsAreaEl.textContent = ''
+  for (i = 0; i < leads.length; i++) {
+    let ulEl = document.createElement('ul')
+    let liEl = document.createElement('li')
+    let anchorEl = document.createElement('a')
+    anchorEl.setAttribute("href", leads[i])
+    anchorEl.textContent = leads[i]
+    // append to dom
+    liEl.append(anchorEl)
+    ulEl.append(liEl)
+    leadsAreaEl.append(ulEl)  
+  }
+ }
